c0aa3a6cc8039f8dcc6f087bf1541bf2464e1b76
Zhaskanat <157293727+zhaskanatt@users.noreply.github.com>
2024-06-02 20:37:16 +0600

    Added Vertex class

---
+++ src/Vertex.java
@@ -0,0 +1,55 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Vertex<T> {
+    private T data; // Data stored in the vertex
+    private Map<Vertex<T>, Double> adjacentVertices; // Adjacent vertices with weights
+
+    public Vertex(T data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void addAdjacentVertex(Vertex<T> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public Map<Vertex<T>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    public void setAdjacentVertices(Map<Vertex<T>, Double> adjacentVertices) {
+        this.adjacentVertices = adjacentVertices;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Vertex<?> vertex = (Vertex<?>) o;
+        return Objects.equals(data, vertex.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data);
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+}
